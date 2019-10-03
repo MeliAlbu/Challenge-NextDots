8eec9d7541adf4f4322d69c57947187c63270509
Melina Albuquerque <melinaalbuquerque@MacBook-Pro-de-Melina.local>
2019-10-03 15:26:03 -0300

    Adding Libraries

--- android/app/src/main/java/com/challengedrinks/MainActivity.java
+++ android/app/src/main/java/com/challengedrinks/MainActivity.java
@@ -1,6 +1,11 @@
+
+
 package com.challengedrinks;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+ import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "ChallengeDrinks";
     }
+
+      @Override
+ protected ReactActivityDelegate createReactActivityDelegate() {
+   return new ReactActivityDelegate(this, getMainComponentName()) {
+     @Override
+     protected ReactRootView createRootView() {
+   return new RNGestureHandlerEnabledRootView(MainActivity.this);
+     }
+   };
+ }
 }
