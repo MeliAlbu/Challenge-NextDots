185d5b6b7de714ab8039b95c14b4f0d54d604ca4
Melina Albuquerque <melinaalbuquerque@MacBook-Pro-de-Melina.local>
2019-10-10 11:15:40 -0300

    destructuring props 1

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,14 +3,19 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import styles from './styles';
 
 export default class Card extends Component {
-    render() {
-        return (
+
+
+
+    render() 
+    
+    {const {name} = this.props
+        return (    
            
             <View style={styles.Card}>
                 <Image style=
                 {styles.DrinkImage}
                 source={{ uri: this.props.urlImage }} />
-                <Text style={styles.DrinkName}>{this.props.name}</Text>
+                <Text style={styles.DrinkName}>{name}</Text>
                 <Text>{this.props.idItem}</Text>
             </View>
         )
